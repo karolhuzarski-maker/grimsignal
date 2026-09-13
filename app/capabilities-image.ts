5f001b35cbf44aa839ec8371f9e4f6bda0caa994
karolhuzarski-maker <karol.huzarski@gmail.com>
2026-09-13 23:56:39 +0200

    Assemble capabilities thermal image

---
+++ app/capabilities-image.ts
@@ -0,0 +1,5 @@
+import p0 from "./capabilities-image-parts/p0";
+import p1 from "./capabilities-image-parts/p1";
+import p2 from "./capabilities-image-parts/p2";
+
+export const CAPABILITIES_THERMAL_IMAGE = `data:image/jpeg;base64,${p0}${p1}${p2}`;
